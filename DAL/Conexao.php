a3e0831ed02fb3ffc2d9205b90aaca1fc60f283c
HugoTheBugs <hugoferreira2323@gmail.com>
2024-06-10 22:23:21 -0300

    Update Conexao.php

--- DAL/Conexao.php
+++ DAL/Conexao.php
@@ -1,1 +1,8 @@
-teste
+<?php
+        class Conexao{
+            private static $dbNome = 'concessionaria';
+            private static $dbHost = 'localhost';
+            private static $dbUsuario = 'root';
+            private static $dbSenha = '';
+        }
+?>
